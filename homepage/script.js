900f0d76702365d673af15a52aaac8a0bcfe3935
ikd09 <81186404+ikd09@users.noreply.github.com>
2023-05-21 12:18:17 -0500

    Update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -15,7 +15,7 @@ function lookup() {
       .then(response => response.json())
       .then(data => {
         const textbase = atob(data.content);
-        stringToChunk = textbase.split("\n")
+        stringToChunk = textbase.split("-")
         stringToDisplay = getRandomItem(stringToChunk);
         if (stringToDisplay.length < 5) {lookup();}
         document.getElementById("qotd").innerHTML = stringToDisplay;
